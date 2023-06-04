eb762f73c5165ef6e845953fc49541d5c6499493
Rustem Aisariyev <scorus0808@gmail.com>
2023-06-04 12:53:56 +0600

    added some files

--- src/main/java/tacos/entities/Taco.java
+++ src/main/java/tacos/entities/Taco.java
@@ -2,11 +2,20 @@ package tacos.entities;
 
 import java.util.List;
 
+//import jakarta.validation.constraints.NotNull;
+//import jakarta.validation.constraints.Null;
+//import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class Taco {
+	
+	//@NotNull
+	//@Size(min = 5, message = "ame must be at least 5 characters long")
 	private String name;
+	
+	//@NotNull
+	//@Size(min = 1, message = "You must choose at least 1 ingredient")
 	private List<Ingredient> ingredients;
 	
 }
